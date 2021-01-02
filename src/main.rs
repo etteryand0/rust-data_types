9f14315b0cbece330cbcce4e05c4eadf7a09fcdc
etteryand0 <etteryand@gmail.com>
2021-01-02 14:21:25 +0900

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,29 @@ fn constants() {
     println!("{}YOU CAN`T USE mut WITH CONSTANTS!!!\n{}", RED, CLEAR);
 }
 
+fn shadowing() {
+    println!("{}Let`s learn shadowing{}", YELLOW, CLEAR);
+    println!("You can shadow mutable/immutable variables by using {}let{}", GREEN, CLEAR);
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("{}x{} = {}", GREEN, CLEAR, x);
+
+    println!("Or you can reassign");
+    let mut x = 5;
+    x = x + 4;
+    x = x * 2;
+    println!("{}mut x{} = {}", GREEN, CLEAR, x);
+
+    println!("Shadowing is useful when you want to change the data type but use the same variable name");
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("{}spaces{} = {}", GREEN, CLEAR, spaces);
+    println!("{}spaces{} was string before. Now it is integer", GREEN, CLEAR);
+}
 
 fn main() {
     println!("{}Hello, data types!", GREEN);
     constants();
+    shadowing();
 }
